3421380bc5ac120aab57941ad1b5ef3dc8828748
KrushnaBodake <bodakekrushna9@gmail.com>
2023-03-09 17:53:19 +0530

    Add project files.

---
+++ FillterDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FillterDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
